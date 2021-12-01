575558377a3972cb35e38d5747e8959ce35d8f69
Matheus Anjo <matheus.anjo.andre@gmail.com>
2021-12-01 18:52:30 -0300

    done

--- index.js
+++ index.js
@@ -5,18 +5,50 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const DB_NAME = `Recipe`;
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+  .connect(`mongodb://localhost:27017/${DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .then(() => {
+  .then(async (connObj) => {
     // Run your code here, after you have insured that the connection was made
+    console.log(`Conectado ao banco ${connObj.connections[0].name} com SUCESSAGEM`);
+    try{
+      
+      const creatRecipe = await Recipe.create({
+
+        title:"Hamburguer de miojo",
+        level: "Easy Peasy",
+        ingredients: ["miojo", "hamburguer", "cheddar", "1 ovo", "cebolinha", "bacon"],
+        cusine: "Brasileira",
+        dishType: "snack",
+        duration: 11,
+        creator: "Otaviao",
+        created: "2013-08-15",
+
+      });
+
+      await Recipe.insertMany(data);
+
+      await Recipe.findOneAndUpdate(
+        {title:"Chocolate Chip Cookies"},
+        { $set: {level: "UltraPro Chef" }},
+
+        {new: true}
+      );
+
+      await Recipe.deleteOne({
+        title: "Carrot Cake",
+      });
+
+    }
+    catch(err){
+      console.log(err);
+    }
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
